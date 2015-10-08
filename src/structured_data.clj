9b918971ed912c9f0861c7d26548867851c48a88
chris <bearslikehoney@gmail.com>
2015-10-08 11:34:00 +1100

    reworked rectangle exercises, with renewed enthusiasm. added title-length function

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,17 @@
 (ns structured-data)
 
-(defn do-a-thing [x]
-  :-)
+(defn do-a-thing [x] (let [double (+ x x)]
+                       (Math/pow double double)))
 
-(defn spiff [v]
-  :-)
+(defn spiff [v] (if (< (count v) 3)nil (let [first   (get v 0)
+                                             third   (get v 2)]
+                                         (+ first third))))
 
-(defn cutify [v]
-  :-)
+(defn cutify [v] (if (vector? v)
+                   (conj v "<3")
+                   (println "please enter a vector") ))
 
-(defn spiff-destructuring [v]
-  :-)
+(defn spiff-destructuring [[x y z]] (+ x z))
 
 (defn point [x y]
   [x y])
@@ -18,26 +19,25 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn width   [[[x1 y1] [x2 y2]]] (- x2 x1))
 
-(defn height [rectangle]
-  :-)
+(defn height  [[[x1 y1] [x2 y2]]] (- y2 y1))
 
-(defn square? [rectangle]
-  :-)
+(defn square? [[[x1 y1] [x2 y2]]] (if (= (- x2 x1) (- y2 y1))true false))
 
-(defn area [rectangle]
-  :-)
+(defn area    [[[x1 y1] [x2 y2]]] (* (- x2 x1) (- y2 y1)))
 
-(defn contains-point? [rectangle point]
-  :-)
+(defn contains-point? [[[x1 y1] [x2 y2]] [x y]] (and(<= x1 x x2) (<= y1 y y2)))
 
-(defn contains-rectangle? [outer inner]
-  :-)
+;; (if (and (<= x1 x x2) (<= y1 y y2))true false)
+;; note the above line didn't seem to like the if statement
 
-(defn title-length [book]
-  :-)
+(defn contains-rectangle? [[[x1 y1] [x2 y2]] [[x3 y3] [x4 y4]]] (and (<= x1 x3 x4 x2) (<= y1 y3 y4 y2)))
+
+;; (defn contains-rectangle? [outer inner])
+
+
+(defn title-length [] )
 
 (defn author-count [book]
   :-)
